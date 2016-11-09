f5cb53a02a6a4acb1b3bfd81edd027a217b034ab
Filip Kovářík <filipkoss2329@gmail.com>
2016-11-09 17:52:42 +0100

    Upload FixHier, Minitrue

---
+++ FixHier.js
@@ -0,0 +1,17 @@
+﻿FixHier = function(o){
+	let r = function(q,v,s){
+		let [o,p] = FixHier.d[this.$id];
+		switch (typeof q){
+			"undefined": return o;
+			"number": 
+				if(!(o instanceof Array) && Object.keys(o).filter(x=>+x===x).length==0) {
+					let k = o
+				}
+		}
+		
+		}
+	r.$id = FixHier.pullId.next().value;
+	FixHier.d[r.$id] = {};
+}
+FixHier.pullId = (function* pullId(n=0){while(!0)yield n++})()
+FixHier.d = Object.create(null);
---
+++ Ministerstvo pravdy.js
@@ -0,0 +1,19 @@
+﻿// ==UserScript==
+// @name         Ministerstvo pravdy
+// @namespace    
+// @version      0.1
+// @description  Upravte si své známky podle své potřeby: varování: používejte jen pokud budete mít !stejné! nastavení na všech zařízení, kam se můžou klamané osoby dostat!
+// @author       Glen Elg, Franziska von Karma
+// @match        http://isas.gjs.cz/prubezna-klasifikace.php*
+// @grant        none
+// @require      http://code.jquery.com/jquery-1.11.0.min.js
+// ==/UserScript==
+
+(function(L,Lk,$s,$p){
+$l=_=>j=$p(L[Lk])||{g:{}};
+$s=_=>L[Lk]=$s(j);
+$r=_=>
+$G=_=>{r:0}
+j.h=0; //hide new marks
+    
+})(localStorage,"Minitrue",JSON.stringify,JSON.parse);
